74c9441c53756dfacdd9e233171ea92f4fd3fd51
lawrence8 <laurentiu16m@yahoo.com>
2015-02-26 11:33:04 -0500

    b

--- digital-template-gh-pages/js/main.js
+++ digital-template-gh-pages/js/main.js
@@ -1,52 +1,222 @@
 window.onload = function() {
-    // You might want to start with a template that uses GameStates:
-    //     https://github.com/photonstorm/phaser/tree/master/resources/Project%20Templates/Basic
     
-    // You can copy-and-paste the code from any of the examples at http://examples.phaser.io here.
-    // You will need to change the fourth parameter to "new Phaser.Game()" from
-    // 'phaser-example' to 'game', which is the id of the HTML element where we
-    // want the game to go.
-    // The assets (and code) can be found at: https://github.com/photonstorm/phaser/tree/master/examples/assets
-    // You will need to change the paths you pass to "game.load.image()" or any other
-    // loading functions to reflect where you are putting the assets.
-    // All loading functions will typically all be found inside "preload()".
     
-    "use strict";
+   
+
+
+var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+
+function preload() {
+
+    game.load.image('sky', 'assets/pics/sky.png');
+    game.load.image('ground', 'assets/pics/platform.png');
+    game.load.image('star', 'assets/pics/b.png');
+    game.load.spritesheet('dude', 'assets/pics/index.png');
+	game.load.audio('a', 'assets/5.mp3');
+
+}
+
+var player;
+var platforms;
+var cursors;
+
+var stars;
+var score = 0;
+var scoreText;
+var a;
+function create() {
+
+
+    game.physics.startSystem(Phaser.Physics.ARCADE);
+
+   
+    game.add.sprite(0, 0, 'sky');
+
+   
+    platforms = game.add.group();
+
+  
+    platforms.enableBody = true;
+
     
-    var game = new Phaser.Game( 800, 600, Phaser.AUTO, 'game', { preload: preload, create: create, update: update } );
+    var ground = platforms.create(0, game.world.height - 64, 'ground');
+
     
-    function preload() {
-        // Load an image and call it 'logo'.
-        game.load.image( 'logo', 'assets/phaser.png' );
-    }
+    ground.scale.setTo(2, 2);
+
     
-    var bouncy;
+    ground.body.immovable = true;
+
+  
+    var ledge = platforms.create(400, 400, 'ground');
+	
+    ledge.body.immovable = true;
+	ledge.scale.setTo(0.5, 0.5);
+
+    ledge = platforms.create(0, 400, 'ground');
+    ledge.body.immovable = true;
+	ledge.scale.setTo(0.5, 0.5);
+	
+	ledge = platforms.create(100, 300, 'ground');
+    ledge.body.immovable = true;
+	ledge.scale.setTo(1, 0.5);
+	
+	ledge = platforms.create(650, 300, 'ground');
+    ledge.body.immovable = true;
+	ledge.scale.setTo(0.5, 0.5);
+	
+	ledge = platforms.create(500, 200, 'ground');
+    ledge.body.immovable = true;
+	ledge.scale.setTo(0.5, 1);
+	
+	ledge = platforms.create(200, 200, 'ground');
+    ledge.body.immovable = true;
+	ledge.scale.setTo(1, 1);
+	
+	ledge = platforms.create(-100, 200, 'ground');
+    ledge.body.immovable = true;
+	ledge.scale.setTo(0.5, 0.5);
+	
+
+   
+    player = game.add.sprite(32, game.world.height - 150, 'dude');
+	player.scale.setTo(0.2, 0.2);
+
+   
+    game.physics.arcade.enable(player);
+
+    player.body.bounce.y = 0.2;
+    player.body.gravity.y = 300;
+    player.body.collideWorldBounds = true;
+
     
-    function create() {
-        // Create a sprite at the center of the screen using the 'logo' image.
-        bouncy = game.add.sprite( game.world.centerX, game.world.centerY, 'logo' );
-        // Anchor the sprite at its center, as opposed to its top-left corner.
-        // so it will be truly centered.
-        bouncy.anchor.setTo( 0.5, 0.5 );
+ 
+
+    
+    stars = game.add.group();
+
+    stars.enableBody = true;
+
+ 
+    
+		
+		
+   
+   var rand = game.rnd.realInRange(100, 500);
         
-        // Turn on the arcade physics engine for this sprite.
-        game.physics.enable( bouncy, Phaser.Physics.ARCADE );
-        // Make it bounce off of the world bounds.
-        bouncy.body.collideWorldBounds = true;
+        var star = stars.create(rand, rand, 'star');
+		star.scale.setTo(0.2, 0.2);
+
         
-        // Add some text using a CSS style.
-        // Center it in X, and position its top 15 pixels from the top of the world.
-        var style = { font: "25px Verdana", fill: "#9999ff", align: "center" };
-        var text = game.add.text( game.world.centerX, 15, "Build something awesome.", style );
-        text.anchor.setTo( 0.5, 0.0 );
+
+       
+        star.body.gravity.y = 300;
+
+       
+        star.body.bounce.y = 0.7 + Math.random() * 0.2;
+    
+   
+   
+
+    
+
+    
+    scoreText = game.add.text(16, 16, 'Collect the documents    Score: 0', { fontSize: '32px', fill: '#000' });
+
+    cursors = game.input.keyboard.createCursorKeys();
+	
+	a = game.add.audio('a');
+	a.volume=0.08;
+	 a.allowMultiple = true;
+    
+}
+
+function update() {
+   
+
+    game.physics.arcade.collide(player, platforms);
+    game.physics.arcade.collide(stars, platforms);
+
+    
+    game.physics.arcade.overlap(player, stars, collectStar,collectStar2);
+
+    
+    player.body.velocity.x = 0;
+	if(score>90){
+		scoreText.text = 'GOOD JOB, YOU CAN GET HIRED NOW' ;
+		}
+	
+	
+	
+	
+	
+
+    if (cursors.left.isDown)
+    {
+        
+        player.body.velocity.x = -150;
+
+        
+    }
+    else if (cursors.right.isDown)
+    {
+        
+        player.body.velocity.x = 150;
+
+        
+    }
+    else
+    {
+        
+        player.animations.stop();
+
+        
     }
     
-    function update() {
-        // Accelerate the 'logo' sprite towards the cursor,
-        // accelerating at 500 pixels/second and moving no faster than 500 pixels/second
-        // in X or Y.
-        // This function returns the rotation angle that makes it visually match its
-        // new trajectory.
-        bouncy.rotation = game.physics.arcade.accelerateToPointer( bouncy, this.game.input.activePointer, 500, 500, 500 );
+   
+    if (cursors.up.isDown && player.body.touching.down)
+    {
+        player.body.velocity.y = -350;
     }
+
+}
+
+function collectStar (player, star) {
+   
+   a.play();
+  
+    star.kill();
+
+    score += 10;
+    scoreText.text = 'Collect the documents     Score: ' + score;
+
+}
+
+
+function collectStar2 (player, star) {
+   
+    if (score < 90){
+	 stars = game.add.group();
+
+    
+    stars.enableBody = true;
+   
+   var rand = game.rnd.realInRange(100, 500);
+       
+        var star = stars.create(rand, rand, 'star');
+		star.scale.setTo(0.2, 0.2);
+
+        
+
+        
+        star.body.gravity.y = 300;
+
+        star.body.bounce.y = 0.7 + Math.random() * 0.2;
+    
+   
+   }
+  
+   
+   }
+
 };
